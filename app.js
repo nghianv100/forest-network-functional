81276ef9d708fbe1d4889267c88275c0cb11d627
Nghia. Nguyen Van (2) <nghinv2@vng.com.vn>
2018-12-25 10:01:08 +0700

    set header

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let unfollowController = require('./controllers/UnfollowController');
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
     next();
 })
 
